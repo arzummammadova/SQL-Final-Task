aa6889834e9bee26761833b0b69a25f1fe8e17f5
Arzu Mammadova <arzuam-af106@code.edu.az>
2025-08-22 18:28:05 +0400

    Update finaltask.sql
    ilk 10 tap

--- finaltask.sql
+++ finaltask.sql
@@ -24,3 +24,34 @@ select * from employees where department_id in(50,80,90)and (first_name like '%a
 --çoxdan aza do?ru s?ralay?b ilk 5 s?trind?n ba?qa dig?rl?rini g?tirin. 
 select * from employees where salary<20000 order by salary desc fetch first 5 rows only
 
+
+
+--7Employees cədvəlində maaşı 7000 dən çox olan və department_id- i 50, 60 və ya 80 olan əməkdaşları gətirin.
+--İN istifadə olunmasın. 
+
+select * from employees where salary>7000 and (department_id=50 or department_id=60 or department_id=80)
+
+
+
+--8Employees cədvəlində əməkdaşların adını, soyadını, commission_pct, phone_number və department_name gətirin.
+--Phone_number-də ‘.’ –i ‘–‘ lə əvəz edin. Commission_pct-də null olanlar ilk gəlsin. 
+
+select first_name,last_name,commission_pct,replace(phone_number,'.','-') from employees order by  commission_pct
+nulls first 
+
+--9Employees cədvəlində əməkdaşalrın adını, soyadını, maaşını və maaşın çoxdan aza doğru sıra nömrəsini gətirin. 
+--Eyni maaşda olanlara eyni sıra nömeəsi yazılsın. 
+
+select first_name,last_name,salary,
+dense_rank() over(order by salary desc) as rankk
+
+from employees order by salary desc
+
+
+
+--10Employees cədvəlində maaşı 10000 dən çox olan və şəhəri 
+--Oxford olan əməkdaşların adını, maaşını, department_id-i, department_name-i və şəhər adını gətirin. 
+select e.first_name,e.salary,e.department_id,d.department_name ,l.city from employees e
+join  departments d  on e.department_id=d.department_id 
+join locations l on l.location_id=d.location_id where e.salary>10000
+
